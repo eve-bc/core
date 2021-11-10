19ac7fbd0a44677574f03a5f02e4102e25491b49
Ictao <ictao@pop-os.localdomain>
2021-11-11 00:03:43 +0100

    add another test

--- src/test/core.test.ts
+++ src/test/core.test.ts
@@ -1,6 +1,7 @@
 import {ServiceBroadcast} from "../modules/service-broadcast/service-broadcast.class";
 import {ConsolePlugin} from "../plugins/console/console-plugin.class";
 import {BroadcastType} from "../types/broadcast-type.enum";
+import {InMemoryStorePlugin} from "../plugins/in-memory-store/in-memory-store-plugin.class";
 
 test('simulate a few with console output', async () => {
 	const bc = new ServiceBroadcast('demo-service');
@@ -12,4 +13,26 @@ test('simulate a few with console output', async () => {
 		recipient: { type: 'Database', name: 'default-sql' },
 		metadata: { query: 'SELECT 1 = 1' }
 	});
+});
+
+test('simulate a few with in memory output', async () => {
+	const bc = new ServiceBroadcast('demo-service');
+	const mem = new InMemoryStorePlugin();
+	bc.usePlugin(mem);
+
+	await bc.announce({
+		type: BroadcastType.ATTEMPT,
+		message: 'fetch a user from a database',
+		recipient: { type: 'Database', name: 'default-sql' },
+		metadata: { query: 'SELECT 1 = 1' }
+	});
+
+	await bc.announce({
+		type: BroadcastType.SUCCESS,
+		message: 'fetch a user from a database',
+		recipient: { type: 'Database', name: 'default-sql' },
+		metadata: { query: 'SELECT 1 = 1' }
+	});
+	
+	expect(mem.db.length).toBe(2);
 });
